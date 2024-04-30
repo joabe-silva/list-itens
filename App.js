f074c80cebe85588e2e2a81cea358cbc0a9eaa78
Thiago Lima <thiagolimadeoliveira18@gmail.com>
2024-04-30 09:36:00 -0300

    ajuste na importacao do react-native-paper

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './src/Login';
+import { PaperProvider } from "react-native-paper";
+import Routes from "./src/routes";
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Login />
-    </View>
+    <PaperProvider>
+      <Routes />
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
