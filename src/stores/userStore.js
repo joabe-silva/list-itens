98b2cc41d636433bf73933f5eb021fcec90c5405
Thiago Lima <thiagolimadeoliveira18@gmail.com>
2024-05-13 10:52:04 -0300

    add authenticated user global storage

---
+++ src/stores/userStore.js
@@ -0,0 +1,8 @@
+import {create} from 'zustand'
+
+const useUserStore = create((set) => ({
+  authenticatedUser: {},
+  setAuthenticatedUser: (user) => set({authenticatedUser: user}),
+}))
+
+export default useUserStore;
